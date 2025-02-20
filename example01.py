623bb166cf6cf3ccb63ce896ed886820dd0ced2c
chalermsak-s <kaidohjung@gmail.com>
2025-02-20 14:13:25 +0700

    Update example01.py

---
+++ example01.py
@@ -0,0 +1,9 @@
+# Example 1 ตัวอย่างการดำเนินการทางคณิตศาสตร์
+a = 5
+b = 3
+print("Addition:", a + b)  # บวก
+print("Subtraction:", a - b)  # ลบ
+print("Multiplication:", a * b)  # คูณ
+print("Division:", a / b)  # หาร
+print("Exponent:", a ** b)  # ยกกำลัง
+print("Square Root:", a ** 0.5)  # รากที่สอง
