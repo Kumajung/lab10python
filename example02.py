b4298a700a431a2d101d147ba10a61bf5a37503a
chalermsak-s <kaidohjung@gmail.com>
2025-02-20 14:15:39 +0700

    2-3 python

---
+++ example02.py
@@ -0,0 +1,3 @@
+# Example 2 ตัวอย่างการใช้ PEMDAS
+expression = 5 * 4 - 2
+print("Result of 5 * 4 - 2:", expression)  # ผลลัพธ์: 18
---
+++ example03.py
@@ -0,0 +1,7 @@
+# Example 3 ตัวอย่างการใช้ Relational Operators
+x = 10
+y = 20
+print("x < y:", x < y)  # น้อยกว่า
+print("x > y:", x > y)  # มากกว่า
+print("x == y:", x == y)  # เท่ากับ
+print("x != y:", x != y)  # ไม่เท่ากับ
